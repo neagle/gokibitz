5eec51a9631e960794fc6f18a92dead54bbc543a
Nate Eagle <nate@nateeagle.com>
2015-01-15 23:09:00 -0500

    make footer height dynamic

---
+++ client/src/js/directives/dynamicHeightFooter.js
@@ -0,0 +1,22 @@
+angular.module('gokibitz.directives')
+.directive('footer', function (debounce, $window, $document) {
+		return {
+			restrict: 'E',
+			link: function(scope, element, attrs) {
+				var $body = angular.element($document.body);
+
+				// Set the margin-bottom on body to accomodate a footer of arbitrary height
+				function setMargin() {
+					var height = element.outerHeight(true);
+					$document[0].body.setAttribute('style', 'margin-bottom: ' + height + 'px');
+				}
+
+				$document.ready(function () {
+					setMargin();
+				});
+
+				var w = angular.element($window);
+				w.bind('resize', debounce(1000, setMargin));
+			}
+		};
+});
