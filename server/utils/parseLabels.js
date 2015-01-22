b18448d172b564433d016c5d0b72b3df0419cba0
Nate Eagle <nate@nateeagle.com>
2015-01-22 14:03:06 -0500

    Fix extra whitespace in sequences bug, closes #16

--- server/utils/parseLabels.js
+++ server/utils/parseLabels.js
@@ -2,7 +2,7 @@ module.exports = function (markdown) {
 	var output;
 
 	// Wrap sequences of moves with sequence tags and moves with move tags
-	// @see http://www.regexr.com/3a8ft
+	// @see http://www.regexr.com/3a93a
 	var parser = RegExp([
 		// Capture the first move color
 		'([wW]hite[\\s]?|[bB]lack[\\s]?|[wWbB][\\s]?)?',
@@ -28,7 +28,7 @@ module.exports = function (markdown) {
 			// I detest having a trailing space inside the tag rather than outside it
 			// If I can write the regex to avoid this step, please let me know
 			if (str.substring(str.length - 1) === ' ') {
-				str = str.substring(0, str.length - 1);
+				str = str.trim();
 				space = ' ';
 			}
 
@@ -44,7 +44,7 @@ module.exports = function (markdown) {
 			// Normalize the format of the sequence for easy parsing on the client side
 			var sequence = '';
 			if (type === 'sequence') {
-				sequence= ' sequence="' + str.replace(/[,; ]+/g, '-').toUpperCase() + '"';
+				sequence= ' sequence="' + str.trim().replace(/[,; ]+/g, '-').toUpperCase() + '"';
 			}
 
 			return '<sgf-label' + firstMove + sequence + '>' + color + str + '</sgf-label>' + space;
