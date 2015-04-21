940f344011df1db96a20c7818f99fe93f04bc6fc
Nate Eagle <n.eagle@gmail.com>
2015-04-21 11:02:34 -0400

    in-progress

---
+++ client/src/js/directives/sticky.js
@@ -0,0 +1,18 @@
+angular.module('gokibitz.directives')
+.directive('sticky', function ($window, debounce) {
+	return {
+		restrict: 'A',
+		link: function ($scope, elem, attrs) {
+			function resize() {
+			}
+
+			function scroll() {
+				lo
+			}
+
+			var $win = angular.element($window);
+
+			$win.bind('scroll',
+		}
+	};
+});
