43512f8cb3ec9f1b974b12512171c94517d33e80
Nate Eagle <nate@nateeagle.com>
2015-04-21 16:12:35 -0400

    Add sticky behavior on scroll to SGFs

--- client/src/js/directives/sticky.js
+++ client/src/js/directives/sticky.js
@@ -1,18 +1,73 @@
+/**
+ * Custom sticky behavior for SGFs
+ */
 angular.module('gokibitz.directives')
-.directive('sticky', function ($window, debounce) {
+.directive('sticky', function ($window, $document, debounce) {
 	return {
 		restrict: 'A',
 		link: function ($scope, elem, attrs) {
+			// There are a lot of hard-coded values in this. I'll pay for it later.
+			var $win = angular.element($window);
+			var document = $document[0];
+			var doc = document.documentElement;
+
+			// Allocate space for the header and the footer
+			var topSpace = 70;
+			var bottomSpace = 140;
+
+			// Cross-browser getDocHeight function
+			// (I miss jQuery.)
+			function getDocHeight() {
+				return Math.max(
+					document.body.scrollHeight, document.documentElement.scrollHeight,
+					document.body.offsetHeight, document.documentElement.offsetHeight,
+					document.body.clientHeight, document.documentElement.clientHeight
+				);
+			}
+
+			// Turn scroll behavior off for narrower widths
 			function resize() {
+				if ($window.innerWidth >= 992) {
+					$win.on('scroll', scroll);
+				} else {
+					$win.off('scroll');
+					elem[0].style.position = null;
+					elem[0].style.top = 0;
+					elem[0].style.bottom = null;
+				}
 			}
 
 			function scroll() {
-				lo
+				var rect = elem[0].getBoundingClientRect();
+				var left = ($window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+				var top = ($window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+				var elemHeight = elem[0].offsetHeight;
+				var totalHeight = top - topSpace;
+
+				if (top >= topSpace) {
+					elem[0].style.position = 'fixed';
+					elem[0].style.top = topSpace + 'px';
+					elem[0].style.bottom = null;
+
+					var bottomStick = getDocHeight() - topSpace - bottomSpace - elemHeight;
+
+					if (totalHeight > bottomStick) {
+						elem[0].style.position = 'absolute';
+						elem[0].style.top = bottomStick + 'px';
+					}
+				} else {
+					elem[0].style.position = 'relative';
+					elem[0].style.top = 0;
+					elem[0].style.bottom = null;
+				}
 			}
 
-			var $win = angular.element($window);
+			$win.on('resize', debounce(20, resize));
 
-			$win.bind('scroll',
+			$document.ready(function () {
+				resize();
+			});
 		}
 	};
 });
