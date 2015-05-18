ef52b9486cb2e28aa3e93ce61866f75322dbd33e
sphaso <sphaso@gmail.com>
2015-05-18 20:19:14 +0200

    #82 created filter and added to page title and top labels in kifu

---
+++ client/src/js/filters/rank.js
@@ -0,0 +1,25 @@
+angular.module('gokibitz.filters')
+    .filter('rank', function () {
+	return function (input) {
+        var number = /^[0-9]+/.exec(input);
+        var qualification = /[a-zA-Z]+/.exec(input);
+
+        if(!number || !qualification){
+            return '';
+        }
+
+        var lowerQualification = qualification[0].toLowerCase();
+
+        if(lowerQualification.indexOf("k") > -1){
+            return number[0] + "k";
+        }
+        if(lowerQualification.indexOf("d") > -1){
+            return number[0] + "d";
+        }
+        if(lowerQualification.indexOf("p") > -1){
+            return number[0] + "p";
+        }
+
+        return '';
+    }
+});
