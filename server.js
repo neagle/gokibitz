1f23a14620fb8d140cee0e04b469834d2eaddc64
Nate Eagle <nate@nateeagle.com>
2015-05-19 16:01:32 -0400

    Test Travis CI integration

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
