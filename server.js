0d0d2926b2209383fb8257736f8665bed977923b
Nate Eagle <n.eagle@gmail.com>
2015-07-23 10:48:37 -0400

    Remove console.log

--- server.js
+++ server.js
@@ -86,7 +86,6 @@ app.use(function (req, res, next) {
 
 /// error handlers
 
-//console.log('app.get("env")', app.get("env"));
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
